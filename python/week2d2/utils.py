b0d0be6b8763f7f3196e8ea60ec73a6c9c44525e
Gunnvant <gunnvant3.14@gmail.com>
2022-08-18 18:48:29 +0530

    oops intro codes pushed

--- python/week2d2/utils.py
+++ python/week2d2/utils.py
@@ -31,7 +31,7 @@ def summary(data:list)->dict:
 def dtypes(data:list)->dict:
     row = data[1]
     schema = {}
-    for k,v in row:
+    for k,v in row.items():
         if type(v)!=str:
             schema[k] = str(type(v))
         else:
@@ -40,3 +40,45 @@ def dtypes(data:list)->dict:
             else:
                 schema[k] = 'str'
     return schema
+
+class IO:
+    
+    def read_csv(self,path:str,delim:str)->list:
+        with open(path,'r') as f:
+            reader = csv.DictReader(f,delimiter=delim)
+            data = []
+            for row in reader:
+                data.append(row)
+        return data
+    
+    def write_csv(self,path:str,data:list,headers:Union[str,None]=None):
+        with open(path,'w') as f:
+            if headers is None:
+                headers = list(data[0].keys())
+            writer = csv.DictWriter(f,fieldnames=headers)
+            writer.writeheader()
+            for row in data:
+                writer.writerow(row)
+
+class Dataset:
+    def __init__(self,data:list):
+        self.data = data
+
+    def summary(self)->dict:
+        n_cols = len(self.data[0].keys())
+        n_rows = len(self.data)
+        col_names = list(self.data[0].keys())
+        return {'n_cols':n_cols,'n_rows':n_rows,'col_names':col_names}
+
+    def dtypes(self)->dict:
+        row = self.data[1].items()  
+        schema = {}
+        for k,v in row:
+            if type(v)!=str:
+                schema[k] = str(type(v))
+            else:
+                if v.isnumeric():
+                    schema[k] = 'num'
+                else:
+                    schema[k] = 'str'
+        return schema
