9f1e6de75d7f7e5b785ed001229200b5dd5042b6
Gunnvant <gunnvant3.14@gmail.com>
2023-02-09 15:20:46 +0530

    STY:Removed redundant config declarations

--- python/interview_prep/sql/bulk_load/src/load.py
+++ python/interview_prep/sql/bulk_load/src/load.py
@@ -1,14 +1,6 @@
 import psycopg2
 import src.config as config
 
-user = config.user
-pw = config.password
-port = config.port
-host = config.host
-dbname = config.dbname
-file_path = config.file_path
-table_name = config.tablename
-
 def dump_data(config=config):
     conn = psycopg2.connect(dbname = config.dbname,
                             user = config.user,
